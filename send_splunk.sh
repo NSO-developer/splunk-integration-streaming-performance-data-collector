f1c650b66b0dc99253b83e94fb70e0f6781f5c63
Qi Li <leeli4@cisco.com>
2024-10-11 16:54:49 +0200

    splunk sender bug

--- send_splunk.sh
+++ send_splunk.sh
@@ -2,8 +2,8 @@
 
 #python --version
 #python -m pip install -r requirments.txt
-echo $2" "$3" "$4 >> data/final.dat
+echo $2" "$3" "$4" "$5 >> data/final.dat
 
-python lib/splunk.py  $1 $2 $3 $4
+python lib/splunk.py  $1 $2 $3 $4 $5 
 
 
